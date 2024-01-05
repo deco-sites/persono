ba64a0ad02d7f654db4ef5283f7723630580e55d
Sacci <saccilucas@gmail.com>
2024-01-05 16:30:01 -0300

    feat(packs): productlist props added

--- packs/constants.ts
+++ packs/constants.ts
@@ -1,2 +1,3 @@
 export const AMMO_DEVICE_ID_HEADER = "X-Ammo-Device-Id";
 export const AMMO_TOKEN_HEADER = "X-Ammo-Token";
+export const DECO_CACHE_OPTION = "stale-while-revalidate";
--- packs/loaders/productList.ts
+++ packs/loaders/productList.ts
@@ -1,14 +1,34 @@
 import type { Props as AppContext } from "deco-sites/persono/apps/site.ts";
 import type { Product } from "apps/commerce/types.ts";
 import paths from "$store/packs/utils/paths.ts";
+/* import { DecoRequestInit, fetchAPI } from "apps/utils/fetch.ts";
+import { DECO_CACHE_OPTION } from "$store/packs/constants.ts"; */
+
+export interface Props {
+  /**
+   * @title SKU
+   * @description Listar produtos relacionados com esta SKU.
+   */
+  sku?: string;
+  /**
+   * @title Categoria
+   * @description Listar produtos presentes nesta categoria. É possível usar filtros de categoria personalizados
+   */
+  category: Category;
+}
+
+interface Category {
+  path: string;
+}
 
 /**
  * @title Ammo Varejo - Listagem de Produtos
+ * @description Só é possível usar uma propriedade por vez
  */
 const loader = async (
-  _props: unknown,
+  _props: Props,
   _req: Request,
-  ctx: AppContext
+  ctx: AppContext,
 ): Promise<Product[] | null> => {
   const { publicUrl } = ctx;
   const path = paths(publicUrl!);
