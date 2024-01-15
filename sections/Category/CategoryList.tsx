f9a10fe8e6adb3fadf0e9d92e2683a78a69c7940
Suyndara <souzamatheussilva41@gmail.com>
2024-01-15 13:59:58 -0300

    implementação da category list;
    adição dos botões laterais;
    
    adição do gradient por cima das imagens;
    
    implementação da responsividade;

--- sections/Category/CategoryList.tsx
+++ sections/Category/CategoryList.tsx
@@ -1,6 +1,7 @@
 import Header from "$store/components/ui/SectionHeader.tsx";
 import Slider from "$store/components/ui/Slider.tsx";
 import SliderJS from "$store/islands/SliderJS.tsx";
+import Icon from "$store/components/ui/Icon.tsx";
 import { useId } from "$store/sdk/useId.ts";
 import Image from "apps/website/components/Image.tsx";
 import type { ImageWidget } from "apps/admin/widgets.ts";
@@ -30,7 +31,7 @@ export interface Props {
 }
 
 function CardText(
-  { tag, label, description, alignment }: {
+  { tag, label, alignment }: {
     tag?: string;
     label?: string;
     description?: string;
@@ -39,13 +40,11 @@ function CardText(
 ) {
   return (
     <div
-      class={`flex flex-col ${
+      class={`flex flex-col text-center${
         alignment === "center" ? "text-center" : "text-left"
       }`}
     >
-      {tag && <div class="text-sm text-primary">{tag}</div>}
-      {label && <h3 class="text-lg text-base-content">{label}</h3>}
-      {description && <div class="text-sm text-neutral">{description}</div>}
+      {tag && <div class="text-sm text-secondary">{tag}</div>}
     </div>
   );
 }
@@ -80,22 +79,38 @@ function CategoryList(props: Props) {
   return (
     <div
       id={id}
-      class="container py-8 flex flex-col gap-8 lg:gap-10 text-base-content  lg:py-10"
+      class="container py-8 flex flex-col gap-8 lg:gap-10 text-base-content lg:py-10"
     >
       <Header
         title={header.title}
-        description={header.description || ""}
+      // description={header.description || ""} //
         alignment={layout.headerAlignment || "center"}
       />
 
-      <Slider class="carousel carousel-start gap-4 lg:gap-8 row-start-2 row-end-5">
+      <Slider class="flex justify-center items-center carousel carousel-start gap-6 lg:gap-8 row-start-2 row-end-5 relative">
+        <div class="z-10 col-start-1 row-start-2 absolute left-2 lg:left-2 2xl:left-32 md:hidden rounded-full bg-white">
+          <Slider.PrevButton class="btn btn-circle bg-white cursor-pointer"
+          
+          style={{
+            border: '1px solid #D4DBD7',
+          }}
+          >
+            <Icon
+              class="text-sky-600"
+              size={20}
+              id="ChevronLeft"
+              strokeWidth={2}
+            />
+          </Slider.PrevButton>
+        </div>
+
         {list.map((
           { tag, label, description, href, image, buttonText },
           index,
         ) => (
           <Slider.Item
             index={index}
-            class="flex flex-col gap-4 carousel-item first:pl-6 sm:first:pl-0 last:pr-6 sm:last:pr-0"
+            class="flex flex-col gap-12 carousel-item first:pl-6 sm:first:pl-0 last:pr-6 sm:last:pr-0"
           >
             <a
               href={href}
@@ -112,15 +127,19 @@ function CategoryList(props: Props) {
                 )}
               {image &&
                 (
-                  <figure>
-                    <Image
-                      class="card w-full"
+                  <figure class='relative'>
+                    <Image 
+                      class="card w-full rounded-3xl relative" 
                       src={image}
                       alt={description || label || tag}
-                      width={160}
-                      height={195}
+                      width={280}
+                      height={268}
                       loading="lazy"
                     />
+
+                  <div class="card w-full absolute bottom-0 rounded-3xl h-full bg-gradient-to-b from-transparent to-black bg-opacity-10">
+                    {label && <h3 class="flex text-lg self-center text-base text-white h-[60px] absolute bottom-1">{label}</h3>}
+                  </div>
                   </figure>
                 )}
               {layout.categoryCard?.textPosition === "bottom" &&
@@ -137,8 +156,23 @@ function CategoryList(props: Props) {
               <a href={href} class="btn">{buttonText}</a>}
           </Slider.Item>
         ))}
-      </Slider>
 
+        <div class="z-10 col-start-3 row-start-2 absolute right-2 lg:right-2 2xl:right-32 md:hidden bg-white rounded-full  cursor-pointer">
+          <Slider.NextButton class="btn btn-circle border-solid border-black bg-white"
+            
+            style={{
+              border: '1px solid #D4DBD7',
+            }}
+          >
+            <Icon
+              class="text-sky-600"
+              size={24}
+              id="ChevronRight"
+              strokeWidth={2}
+            />
+          </Slider.NextButton>
+        </div>
+      </Slider>
       <SliderJS rootId={id} />
     </div>
   );
