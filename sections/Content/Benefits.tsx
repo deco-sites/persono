1369cdd7949799a365ed512c817059b3b6e262f8
Suyndara <souzamatheussilva41@gmail.com>
2024-01-29 16:22:18 -0300

    adição do radius;
    remoção de códigos desnecessários com ajustes nos mesmos;

--- sections/Content/Benefits.tsx
+++ sections/Content/Benefits.tsx
@@ -8,8 +8,7 @@ export interface Props {
     description: string;
   }>;
   layout?: {
-    variation?: "Simple" | "With border" | "Color reverse";
-    headerAlignment?: "center" | "left";
+    headerAlignment?: "center";
   };
 }
 
@@ -30,30 +29,22 @@ export default function Benefits(
       label: "Devolução grátis",
       description: "Veja as condições para devolver seu produto.",
     }],
-    layout,
   } = props;
 
-  const listOfBenefits = benefits.map((benefit, index) => {
-    const showDivider = index < benefits.length - 1;
-    const reverse = layout?.variation === "Color reverse";
-    const benefitLayout = !layout?.variation || layout?.variation === "Simple"
-      ? "tiled"
-      : "piledup";
-
+  const listOfBenefits = benefits.map((benefit) => {
     return (
       <div
-        class={`${
-          reverse ? "bg-primary text-primary-content px-8 py-4" : ""
-        } flex sm:flex-row lg:flex-col gap-4 w-full sm:w-[48%]${
-          benefitLayout == "piledup" ? "flex-col items-center text-center" : ""
-        } ${showDivider && benefitLayout !== "piledup" ? "max-lg:py-6" : ""} ${
-          showDivider ? "lg:pr-8 lg:border-r lg:border-b-0" : ""
-        } ${showDivider && !reverse ? "lg:pb-0" : ""}`}
+        class={`
+          flex sm:flex-row lg:flex-col gap-4 w-full sm:w-[48%] sm:first:pt-0:last:pt-6 
+          ${"lg:flex-col"} 
+          ${"max-lg:py-6"} 
+          ${"lg:pr-8 lg:border-r lg:last:border-r-0"} 
+          ${"lg:pb-0"}`}
       >
         <div class="flex items-center">
           <Icon
             id={benefit.icon}
-            class={reverse ? "text-base-100" : "text-primary"}
+            class="text-base-100"
             width={40}
             height={40}
             strokeWidth={0.01}
@@ -62,16 +53,14 @@ export default function Benefits(
         </div>
         <div class="flex-auto flex flex-col gap-1 lg:gap-2">
           <div
-            class={`text-base lg:text-xl leading-7 font-bold antialiased ${
-              reverse ? "text-base-100" : "text-base-content"
-            }`}
+            class={`text-base lg:text-xl leading-7 font-bold antialiased ${"text-base-content"}`}
           >
             {benefit.label}
           </div>
           <p
-            class={`text-sm leading-5 ${
-              reverse ? "text-base-100" : "text-gray-800"
-            } ${benefitLayout == "piledup" ? "hidden lg:block" : ""}`}
+            class={`text-sm leading-5 
+            ${"text-gray-800"} 
+            ${"lg:block"}`}
           >
             {benefit.description}
           </p>
@@ -82,18 +71,14 @@ export default function Benefits(
 
   return (
     <>
-      {!layout?.variation || layout?.variation === "Simple"
-        ? (
-          <div class="flex w-full container p-6 lg:gap-10 lg:py-10 lg:px-0">
-            <Header
-              alignment={layout?.headerAlignment || "center"}
-            />
-            <div class="flex max-lg:flex-col sm:justify-center sm:items-center w-full max-lg:[&>*:first-child]:pt-0 max-lg:[&>*:last-child]:pt-6 border max-lg:divide-y divide-neutral-300 lg:gap-9 p-10">
-              {listOfBenefits}
-            </div>
-          </div>
-        )
-        : ""}
+      <div class="flex w-full container p-6 lg:gap-10 lg:py-10 lg:px-0">
+        <Header
+          alignment={"center"}
+        />
+        <div class="flex max-lg:flex-col sm:justify-center sm:items-center w-full border max-lg:divide-y divide-neutral-300 lg:gap-9 p-6 lg:p-10 rounded">
+          {listOfBenefits}
+        </div>
+      </div>
     </>
   );
 }
