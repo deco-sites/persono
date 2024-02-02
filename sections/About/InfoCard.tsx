90d03049f8717d2b39d8105ba708a12296ae37e0
Suyndara <souzamatheussilva41@gmail.com>
2024-02-01 21:15:28 -0300

    ajuste no banner e na responsividade do layout.

--- sections/About/InfoCard.tsx
+++ sections/About/InfoCard.tsx
@@ -4,34 +4,36 @@ import type { ImageWidget } from "apps/admin/widgets.ts";
 export interface About {
   title?: string;
   description?: string;
-  image: ImageWidget;
+
+  desktop: ImageWidget;
+  mobile: ImageWidget;
 }
 
-function About({ title, description, image }: About) {
+function About({ title, description, mobile, desktop }: About) {
   return (
-    <div class="flex flex-col w-full bg-stone-50">
-      <div class="flex flex-col items-center py-[56px] gap-12">
-        <h1 class="text-2xl font-medium">{title}</h1>
-        <p class="font-normal text-base/[24px] w-[312px]">{description}</p>
+    <div class="flex flex-col items-center w-full bg-[#f8f8f8]">
+      <div class="flex flex-col py-14 gap-12 w-4/5">
+        <h1 class=" sm:w-auto sm:text-center text-2xl font-medium">{title}</h1>
+        <p class="font-normal sm:text-center">{description}</p>
       </div>
-
-      <figure class="h-auto w-full">
-        <Picture>
-          <Source
-            media="(max-width: 380px)"
-            src={image}
-            width={220}
-            height={268}
-          />
-          <img
-            src={image}
-            height={336}
-            width={380}
-            loading="lazy"
-            class="w-full"
-          />
-        </Picture>
-      </figure>
+      <Picture>
+        <Source
+          media="(max-width: 767px)"
+          src={mobile}
+          width={360}
+          height={336}
+        />
+        <Source
+          media="(min-width: 768px)"
+          src={desktop}
+          width={1100}
+          height={600}
+        />
+        <img
+          src={desktop}
+          class="w-full h-full object-cover"
+        />
+      </Picture>
     </div>
   );
 }
