8e8f12a86205046d0279ed4629251cf31240152a
viniciusventura29 <viniciusventura29@gmail.com>
2024-02-03 21:00:13 -0300

    Benefits loaders and component

---
+++ components/ui/Benefits.tsx
@@ -0,0 +1,44 @@
+import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
+import Header from "$store/components/ui/SectionHeader.tsx";
+import { Benefits } from "deco-sites/persono/loaders/Layouts/Benefits.tsx";
+
+export interface Props {
+  benefits?: Benefits[]
+}
+
+export default function Benefits(props: Props) {
+  console.log(props.benefits)
+
+  const listOfBenefits = props.benefits?.map((benefit) => {
+    return (
+      <div
+        class={`flex sm:flex-row lg:flex-col sm:justify-center gap-4 w-full first:pt-0 last:pb-0 h-full max-lg:py-6 lg:pr-8 lg:border-r lg:last:border-r-0 lg:pb-0`}
+      >
+        <div class="flex items-center">
+          <img src={benefit.icon} />
+        </div>
+        <div class="flex flex-col gap-1 lg:gap-2 sm:w-[36%] lg:w-auto">
+          <div
+            class={`text-base lg:text-xl leading-7 font-bold antialiased text-base-content w-full`}
+          >
+            {benefit.label}
+          </div>
+          <p class={`text-sm leading-5 text-gray-800 lg:block`}>
+            {benefit.description}
+          </p>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div class="flex w-full container p-6 lg:gap-10 lg:py-10 lg:px-0">
+        <Header />
+        <div class="flex max-lg:flex-col sm:justify-center sm:items-center w-full border border-[#cccccc] lg:gap-9 p-6 lg:p-10 rounded divide-y lg:divide-y-0 divide-[#cccccc]">
+          {listOfBenefits}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ loaders/Layouts/Benefits.tsx
@@ -0,0 +1,26 @@
+import { ImageWidget } from "apps/admin/widgets.ts";
+
+export interface Benefits {
+  icon: ImageWidget;
+  label: string;
+  description: string;
+}
+
+interface Props {
+  benefits: Benefits[];
+}
+
+/** @title Benefits */
+const loader = ({ benefits }: Props): Benefits[] => benefits;
+
+export const Preview = ({ benefits }: Props) => {
+  return (
+    <div class="h-full w-full grid place-items-center">
+      {benefits.map((b) => (
+        <p>{b.label}</p>
+      ))}
+    </div>
+  );
+};
+
+export default loader;
