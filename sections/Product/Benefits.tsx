edeaf43521fd76b00a1d648877d7d9fae303779c
viniciusventura29 <viniciusventura29@gmail.com>
2024-02-06 18:42:45 -0300

    deno lint format

--- sections/Product/Benefits.tsx
+++ sections/Product/Benefits.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 function containsAnyWord(
   productsNames: string[] | undefined,
-  name: string
+  name: string,
 ): boolean {
   if (!productsNames) return false;
   for (const product of productsNames) {
@@ -20,31 +20,38 @@ function containsAnyWord(
 }
 
 export default function Benefits({ layout, name }: Props) {
-
-  const benefitsHidden = layout.map(l=>containsAnyWord(l.productName, name))
+  const benefitsHidden = layout.map((l) =>
+    containsAnyWord(l.productName, name)
+  );
 
   return (
-    <div class={`max-w-lg ${benefitsHidden.some((value)=>value === true) ? "" : "hidden"}`}>
+    <div
+      class={`max-w-lg ${
+        benefitsHidden.some((value) => value === true) ? "" : "hidden"
+      }`}
+    >
       <h4 class="text-base">
         Benefícios para o sono
       </h4>
       <span class="flex flex-col gap-8 mt-8">
-        {layout.slice(0,3).map((l) =>
-          containsAnyWord(l.productName, name) ? (
-            <div class="flex flex-col gap-2">
-              <div class="flex items-center gap-2">
-                <Image
-                  src={l.icon}
-                  class="text-primary"
-                  width={36}
-                  height={36}
-                  alt={l.label}
-                />
-                <p class="text-md font-bold">{l.label}</p>
+        {layout.slice(0, 3).map((l) =>
+          containsAnyWord(l.productName, name)
+            ? (
+              <div class="flex flex-col gap-2">
+                <div class="flex items-center gap-2">
+                  <Image
+                    src={l.icon}
+                    class="text-primary"
+                    width={36}
+                    height={36}
+                    alt={l.label}
+                  />
+                  <p class="text-md font-bold">{l.label}</p>
+                </div>
+                <p class="text-base">{l.description}</p>
               </div>
-              <p class="text-base">{l.description}</p>
-            </div>
-          ) : null
+            )
+            : null
         )}
       </span>
     </div>
