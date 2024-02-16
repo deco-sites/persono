e512957a8f21d5115ff8db273d470a39e2d9f018
SACCI <saccilucas@gmail.com>
2024-02-15 22:57:46 -0300

    feat(product): out of stock feedback

--- components/product/OutOfStock.tsx
+++ components/product/OutOfStock.tsx
@@ -2,6 +2,8 @@ import { useSignal } from "@preact/signals";
 import { invoke } from "$store/runtime.ts";
 import type { Product } from "apps/commerce/types.ts";
 import type { JSX } from "preact";
+import { validateEmail } from "$store/sdk/validateEmail.ts";
+import Icon from "$store/components/ui/Icon.tsx";
 
 export interface Props {
   sku: Product["sku"];
@@ -9,6 +11,8 @@ export interface Props {
 
 function Notify({ sku }: Props) {
   const loading = useSignal(false);
+  const sended = useSignal(false);
+  const sendable = useSignal(false);
 
   const handleSubmit: JSX.GenericEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
@@ -20,15 +24,23 @@ function Notify({ sku }: Props) {
         e.currentTarget.elements.namedItem("email") as RadioNodeList
       )?.value;
 
-      await invoke["deco-sites/persono"].loaders.avaliabilitySubscription({
+      const result = await invoke[
+        "deco-sites/persono"
+      ].loaders.avaliabilitySubscription({
         email,
         sku,
       });
-    } catch (err) {
-      console.log(err);
+
+      sended.value = result ? true : false;
     } finally {
       loading.value = false;
     }
+  };
+
+  const handleInput: JSX.GenericEventHandler<HTMLInputElement> = (e) => {
+    const email = e.currentTarget.value;
+
+    sendable.value = validateEmail(email);
   };
 
   return (
@@ -36,11 +48,29 @@ function Notify({ sku }: Props) {
       <span class="text-base">Este produto está indisponivel no momento</span>
       <span class="text-sm">Avise-me quando estiver disponivel</span>
 
-      <input placeholder="Email" class="input input-bordered" name="email" />
+      <input
+        placeholder="Email"
+        class="input input-bordered"
+        name="email"
+        onChange={handleInput}
+        disabled={sended.value}
+      />
 
-      <button class="btn disabled:loading" disabled={loading}>
-        Enviar
-      </button>
+      {!sended.value && (
+        <button
+          class={`btn ${loading.value ? "loading place-self-center" : ""}`}
+          disabled={!sendable.value}
+        >
+          Enviar
+        </button>
+      )}
+
+      {sended.value && (
+        <span class="place-self-center flex">
+          <Icon width={32} height={32} strokeWidth={1} id={"Check"} />
+          Cadastrado!
+        </span>
+      )}
     </form>
   );
 }
