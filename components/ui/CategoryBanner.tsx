1d401e98a6c09ded928c648ecaa8c5ce573a47e5
viniciusventura29 <viniciusventura29@gmail.com>
2024-02-19 01:07:55 -0300

    Category banner configured in admin

--- components/ui/CategoryBanner.tsx
+++ components/ui/CategoryBanner.tsx
@@ -1,6 +1,9 @@
 import { Picture, Source } from "apps/website/components/Picture.tsx";
 import type { SectionProps } from "deco/types.ts";
 import type { ImageWidget } from "apps/admin/widgets.ts";
+import { Logo } from "deco-sites/persono/components/ui/Logo.tsx";
+import { ProductListingPage } from "apps/commerce/types.ts";
+import NotFound from "deco-sites/persono/sections/Product/NotFound.tsx";
 
 /**
  * @titleBy matcher
@@ -10,8 +13,6 @@ export interface Banner {
   matcher: string;
   /** @description text to be rendered on top of the image */
   title?: string;
-  /** @description text to be rendered on top of the image */
-  subtitle?: string;
   image: {
     /** @description Image for big screens */
     desktop: ImageWidget;
@@ -22,78 +23,71 @@ export interface Banner {
   };
 }
 
-const DEFAULT_PROPS = {
-  banners: [
-    {
-      image: {
-        mobile:
-          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/91102b71-4832-486a-b683-5f7b06f649af",
-        desktop:
-          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/ec597b6a-dcf1-48ca-a99d-95b3c6304f96",
-        alt: "a",
-      },
-      title: "Woman",
-      matcher: "/*",
-      subtitle: "As",
-    },
-  ],
-};
+export interface bannerDefault {
+  image: {
+    /** @description Image for big screens */
+    desktop: ImageWidget;
+    /** @description Image for small screens */
+    mobile: ImageWidget;
+    /** @description image alt text */
+    alt?: string;
+  };
+}
+
+export interface Props {
+  /** @title Banners */
+  banners: Banner[];
+  /** @title Banner Default */
+  bannerDefault: bannerDefault;
+  page: ProductListingPage | null;
+}
 
 function Banner(props: SectionProps<ReturnType<typeof loader>>) {
-  const { banner } = props;
+  const { banner, productBannerCategory, bannerDefault } = props;
 
   if (!banner) {
-    return null;
-  }
-
-  const { title, subtitle, image } = banner;
-
-  return (
-    <div class="grid grid-cols-1 grid-rows-1">
-      <Picture preload class="col-start-1 col-span-1 row-start-1 row-span-1">
-        <Source
-          src={image.mobile}
-          width={360}
-          height={120}
-          media="(max-width: 767px)"
-        />
-        <Source
-          src={image.desktop}
-          width={1440}
-          height={200}
-          media="(min-width: 767px)"
-        />
-        <img class="w-full" src={image.desktop} alt={image.alt ?? title} />
-      </Picture>
-
-      <div class="container flex flex-col items-center justify-center sm:items-start col-start-1 col-span-1 row-start-1 row-span-1 w-full">
-        <h1>
-          <span class="text-5xl font-medium text-base-100">
-            {title}
-          </span>
-        </h1>
-        <h2>
-          <span class="text-xl font-medium text-base-100">
-            {subtitle}
-          </span>
+    return (
+      <div
+        style={{ backgroundImage: `url(${bannerDefault?.image.desktop})` }}
+        class="bg-cover h-28 sm:h-48 w-full bg-base-300 flex items-center justify-between overflow-hidden mb-0 sm:mb-14"
+      >
+        <h2 class="sm:pl-20 pl-6 sm:text-[3.5rem] text-2xl sm:font-normal font-medium text-black">
+          {productBannerCategory ?? "Persono"}
         </h2>
       </div>
+    );
+  }
+
+  const { title, image } = banner;
+
+  return (
+    <div
+      style={{ backgroundImage: `url(${banner?.image.desktop})` }}
+      class="h-48 w-full bg-base-300 flex items-center justify-between overflow-hidden mb-0 sm:mb-14"
+    >
+      <h2 class="pl-20 text-[3.5rem] text-black">
+        {title ?? productBannerCategory}
+      </h2>
     </div>
   );
 }
 
-export interface Props {
-  banners?: Banner[];
-}
-
 export const loader = (props: Props, req: Request) => {
-  const { banners } = { ...DEFAULT_PROPS, ...props };
+  const { banners, page, bannerDefault } = props;
+
+  if (!page) {
+    return {};
+  }
+
+  const productBannerCategory = page.products
+    .map((p) => p.category)[0]
+    ?.split(">")[0];
 
   const banner = banners.find(({ matcher }) =>
     new URLPattern({ pathname: matcher }).test(req.url)
   );
 
-  return { banner };
+  return { banner, productBannerCategory, bannerDefault };
 };
 
 export default Banner;
