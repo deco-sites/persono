da42c0e787755eb53e1514d4e1d8f183674f85bb
Suyndara <souzamatheussilva41@gmail.com>
2024-02-23 20:36:24 -0300

    formatação de arquivo;

--- components/product/PdcGallery.tsx
+++ components/product/PdcGallery.tsx
@@ -4,21 +4,31 @@ import type { ImageWidget } from "apps/admin/widgets.ts";
 export interface Props {
   title?: string;
 
-  image?: ImageWidget;
-  description?: string;
-  label?: string;
-  price?: string;
-  pricedescription?: string;
+  layout: {
+    image?: ImageWidget;
+    description?: string;
+    label?: string;
+    price?: string;
+    pricedescription?: string;
+  };
 }
 
 function PdcGallery(
-  { title, image, description, label, price, pricedescription }: Props,
+  { title, layout }: Props,
 ) {
+
+  const {
+    image,
+    description,
+    label,
+    price,
+    pricedescription,
+  } = layout;
+
   return (
-    <div class="container h-auto w-full flex flex-col items-center">
+    <div class="container h-auto sm:h-[305px] w-full flex flex-col items-center">
       <Header
         title={title}
-        alignment="center"
       />
 
       <div class="w-full h-auto rounded-[40px]">
@@ -30,14 +40,14 @@ function PdcGallery(
             width={307}
           />
 
-          <div class="flex justify-between items-center flex-col bg-black h-auto w-1/2 px-6 py-12">
-            <h3 class="text-white text-2xl font-medium">
+          <div class="flex justify-between items-center flex-col bg-black h-auto w-1/2 px-6 py-6">
+            <h3 class="text-white text-base font-medium">
               {description}
             </h3>
 
             <div class="flex flex-col h-auto w-full text-white">
               <p class="text-xs">{label}</p>
-              <p class="font-medium text-2xl">{price}</p>
+              <p class="font-medium text-base">{price}</p>
               <p class="text-xs">{pricedescription}</p>
             </div>
           </div>
