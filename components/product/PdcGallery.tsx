69431d73470ccb5b1e282e48f75002f3e2688fef
Suyndara <souzamatheussilva41@gmail.com>
2024-02-24 15:33:57 -0300

    alteração geral no padding, tamanho da fonte e largura/altura dos elementos.

--- components/product/PdcGallery.tsx
+++ components/product/PdcGallery.tsx
@@ -34,32 +34,32 @@ function PdcGallery(props: Props) {
   } = props;
 
   return (
-    <div class="container h-auto w-full flex flex-col gap-10">
+    <div class="container flex flex-col h-auto w-full px-6 gap-10">
       <Header
         title={header.title}
         alignment="center"
       />
-      <div class="w-full h-auto justify-center">
+      <div class="flex flex-col lg:grid lg:grid-cols-2 w-full h-auto gap-10">
         {layout.map((
           { image, description, label, price, pricedescription },
         ) => (
-          <div class="flex w-full h-auto md:w-1/2">
+          <div class="flex w-full h-[304px]">
             <img
               src={image}
-              class="w-[60%] h-full object-cover rounded-l-3xl"
+              class="w-[60%] h-auto object-cover rounded-l-[40px]"
               height={307}
               width={307}
             />
 
-            <div class="flex justify-between items-center flex-col bg-black h-auto w-1/2 px-6 py-6 sm:py-12 rounded-r-3xl">
+            <div class="flex justify-between items-center flex-col bg-black h-auto w-[40%] px-6 py-12 rounded-r-[40px]">
               <h3 class="text-white text-base sm:text-2xl font-medium w-full">
                 {label}
               </h3>
 
               <div class="flex flex-col h-auto w-full text-white">
-                <p class="text-xs">{description}</p>
+                <p class="text-sm">{description}</p>
                 <p class="font-medium text-base sm:text-2xl">{price}</p>
-                <p class="text-xs">{pricedescription}</p>
+                <p class="text-sm">{pricedescription}</p>
               </div>
             </div>
           </div>
