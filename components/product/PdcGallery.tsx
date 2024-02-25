135681f8d27d01b0eee1cc30fe704615f14b8f01
Suyndara <souzamatheussilva41@gmail.com>
2024-02-25 03:32:50 -0300

    adição da possibilidade de colocar links nos produtos;

--- components/product/PdcGallery.tsx
+++ components/product/PdcGallery.tsx
@@ -4,6 +4,7 @@ import type { ImageWidget } from "apps/admin/widgets.ts";
 export interface Product {
   image?: ImageWidget;
   label?: string;
+  href?: string;
   description?: string;
   price?: string;
   pricedescription?: string;
@@ -26,6 +27,7 @@ function PdcGallery(props: Props) {
       {
         image: "",
         label: "",
+        href: "",
         description: "",
         price: "",
         pricedescription: "",
@@ -41,28 +43,32 @@ function PdcGallery(props: Props) {
       />
       <div class="flex flex-col lg:grid lg:grid-cols-2 w-full h-auto gap-10">
         {layout.map((
-          { image, description, label, price, pricedescription },
+          { image, description, label, price, href, pricedescription },
         ) => (
-          <div class="flex w-full h-[304px]">
-            <img
-              src={image}
-              class="w-[60%] h-auto object-cover rounded-l-[40px]"
-              height={307}
-              width={307}
-            />
+          <a
+            href={href}
+          >
+            <div class="flex w-full h-[304px]">
+              <img
+                src={image}
+                class="w-[60%] h-auto object-cover rounded-l-[40px]"
+                height={307}
+                width={307}
+              />
 
-            <div class="flex justify-between items-center flex-col bg-black h-auto w-[40%] px-6 py-6 sm:py-12 rounded-r-[40px]">
-              <h3 class="text-white text-base sm:text-2xl font-medium w-full">
-                {label}
-              </h3>
+              <div class="flex justify-between items-center flex-col bg-black h-auto w-[40%] px-6 py-6 sm:py-12 rounded-r-[40px]">
+                <h3 class="text-white text-base sm:text-2xl font-medium w-full">
+                  {label}
+                </h3>
 
-              <div class="flex flex-col h-auto w-full text-white">
-                <p class="text-sm">{description}</p>
-                <p class="font-medium text-base sm:text-2xl">{price}</p>
-                <p class="text-sm">{pricedescription}</p>
+                <div class="flex flex-col h-auto w-full text-white">
+                  <p class="text-sm">{description}</p>
+                  <p class="font-medium text-base sm:text-2xl">{price}</p>
+                  <p class="text-sm">{pricedescription}</p>
+                </div>
               </div>
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </div>
