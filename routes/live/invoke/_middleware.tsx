511a44026b3543fb021069e09c3f3d491ccccaed
SACCI <saccilucas@gmail.com>
2024-03-22 21:37:51 -0300

    feat(packs): token logic setted

--- routes/live/invoke/_middleware.tsx
+++ routes/live/invoke/_middleware.tsx
@@ -1,14 +1,7 @@
 import { getCookies } from "std/http/mod.ts";
 import { FreshContext } from "$fresh/server.ts";
 import { DecoState } from "deco/types.ts";
-import {
-  CookieNames,
-  QueryStringToken as BaseQSToken,
-} from "$store/packs/types.ts";
-
-interface QueryStringToken extends BaseQSToken {
-  rawToken: string;
-}
+import { CookieNames } from "$store/packs/types.ts";
 
 const setCookies = (res: Response, deviceId: string, cookieName: string) => {
   res.headers.append(
@@ -31,35 +24,48 @@ export const handler = async (
   >,
 ) => {
   const res = await ctx.next!();
-  const url = new URL(req.headers.get("referer") ?? req.url);
   const cookies = getCookies(req.headers);
-  //console.log(url);
   const cookieNames = (await ctx.state.invoke(
     "Cookie Config Names",
   )) as CookieNames;
 
   const AMMO_DEVICE_ID_HEADER = cookieNames.ammoDeviceIdCookie;
-  const TOKENS_URL_HEADER = cookieNames.queryStringTokens;
 
-  const queryStringTokens = TOKENS_URL_HEADER.reduce<QueryStringToken[]>(
+  if (cookies[AMMO_DEVICE_ID_HEADER]) return res;
+
+  const url = new URL(req.headers.get("referer") ?? req.url);
+  const TOKENS_URL_HEADER = cookieNames.queryStringTokens;
+  const defaultToken = {
+    [AMMO_DEVICE_ID_HEADER]: crypto.randomUUID(),
+    [cookieNames.creationDateCookie]: new Date().getTime().toString(),
+    [cookieNames.expirationDaysCookie]: "-1",
+  };
+
+  const tokens = TOKENS_URL_HEADER.reduce(
     (acc, { queryStringName, cookieName }) => {
       const rawToken = url.searchParams.get(queryStringName);
       if (!rawToken) {
         return acc;
       }
-      return [...acc, { rawToken, queryStringName, cookieName }];
+
+      const newToken = {
+        [cookieName]: rawToken,
+        [cookieNames.creationDateCookie]: new Date().getTime().toString(),
+        [cookieNames.expirationDaysCookie]: "-1",
+      };
+
+      if (cookieName === AMMO_DEVICE_ID_HEADER) {
+        const index = acc.indexOf(defaultToken);
+        acc[index] = newToken;
+        return acc;
+      }
+
+      return [...acc, newToken];
     },
-    [],
+    [defaultToken],
   );
 
-  if (cookies[AMMO_DEVICE_ID_HEADER]) return res;
-
-
-  const cookieObj = {
-    [AMMO_DEVICE_ID_HEADER]: crypto.randomUUID(),
-    [cookieNames.creationDateCookie]: new Date().getTime().toString(),
-    [cookieNames.expirationDaysCookie]: "-1",
-  };
+  const cookieObj = defaultToken;
 
   setCookies(res, btoa(JSON.stringify(cookieObj)), AMMO_DEVICE_ID_HEADER);
 
