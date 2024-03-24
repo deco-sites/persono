31f228ee07cde4262e75157e4d79e8908cc825b2
Suyndara <souzamatheussilva41@gmail.com>
2024-03-24 03:24:17 -0300

    formatação de arquivo.

--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -71,10 +71,10 @@ function Newsletter({ content, layout = {} }: Props) {
         {content?.description && (
           <div class="lg:max-w-[400px] w-full text-sm">
             {content?.description}&nbsp;
-            {content?.link && (    
+            {content?.link && (
               <b class="text-sm underline font-normal">
                 {content?.link}
-              </b>  
+              </b>
             )}
           </div>
         )}
