126154a626b032b92f5786b7956ce323584d413f
Rodrigo-Barreto <rodicruz10@gmail.com>
2024-03-26 17:30:50 -0300

    chore: update task --no-verify

--- islands/PageFolder.tsx
+++ islands/PageFolder.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "preact/compat";
 import { ComponentChildren } from "preact";
+import { IS_BROWSER } from "$fresh/runtime.ts";
 
 export interface Props {
   children?: ComponentChildren;
@@ -10,7 +11,7 @@ export const PageFolder = ({ children, keepVisible }: Props) => {
   const [showingContent, setCShowingContent] = useState(false);
 
   useEffect(() => {
-    if (showingContent || keepVisible || typeof window === "undefined") {
+    if (showingContent || keepVisible || !IS_BROWSER) {
       return;
     }
 
