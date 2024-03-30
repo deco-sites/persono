f9eb7f8727652aa165056575909e26b2476972ea
Rodrigo-Barreto <rodicruz10@gmail.com>
2024-03-29 23:17:46 -0300

    Added new trasition

--- components/product/ProductInfoCollapse.tsx
+++ components/product/ProductInfoCollapse.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 import type { ComponentChildren } from "preact";
 
 export interface Props {
@@ -6,31 +6,27 @@ export interface Props {
   children?: ComponentChildren;
 }
 
-const ProductInfoCollapse = (
-  { title, children }: Props,
-) => {
+const ProductInfoCollapse = ({ title, children }: Props) => {
   const [openCollapse, setOpenCollapse] = useState(false);
+  const [contentHeight, setContentHeight] = useState(0);
   const [showContent, setShowContent] = useState(false);
+
+  const contentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (openCollapse) {
-      const timeoutId = setTimeout(() => {
-        setShowContent(true);
-      }, 100);
-      return () => clearTimeout(timeoutId);
+      const height = contentRef.current?.scrollHeight;
+      if (height) {
+        setContentHeight(height);
+        setTimeout(() => {
+          setShowContent(true);
+        }, 200);
+      }
     } else {
+      setContentHeight(0);
       setShowContent(false);
     }
   }, [openCollapse]);
-
-  useEffect(() => {
-    if (!showContent) {
-      const timeoutId = setTimeout(() => {
-        setOpenCollapse(false);
-      }, 300);
-      return () => clearTimeout(timeoutId);
-    }
-  }, [showContent]);
 
   return (
     <div class="w-full">
@@ -38,21 +34,18 @@ const ProductInfoCollapse = (
         onClick={() => {
           setOpenCollapse(!openCollapse);
         }}
-        class="flex w-full p-4 items-center justify-between  "
+        class="flex w-full p-4 items-center justify-between"
       >
         <span class="w-full text-start text-base font-normal">{title}</span>
         <span class="text-2xl text-primary">{openCollapse ? "-" : "+"}</span>
       </button>
       <section
-        class={`w-full flex flex-col transition-[max-height] duration-500  ease-linear ${
-          openCollapse ? "max-h-screen " : "max-h-0 "
-        } overflow-hidden`}
+        class={`w-full flex flex-col transition-height duration-500 ease-linear overflow-hidden`}
+        style={{ height: `${contentHeight}px` }}
       >
-        <div
-          class={` px-4 pb-4 ${
-            showContent ? "opacity-100" : "opacity-0 "
-          } transition-opacity duration-700 ease-in-out`}
-        >
+        <div ref={contentRef} class={` ${
+             showContent ? "opacity-100" : "opacity-0 "
+          } transition-opacity duration-700 ease-in-out  px-4 pb-4`}>
           {children}
         </div>
       </section>
@@ -61,3 +54,7 @@ const ProductInfoCollapse = (
 };
 
 export default ProductInfoCollapse;
+
+
+
+
