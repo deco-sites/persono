4f35019b784226bb41e63a683db49055ae2eab29
SACCI <sacci@carcara.tech>
2024-04-22 20:30:23 -0300

    fix(pdp): benefities

--- components/product/ProductInfo.tsx
+++ components/product/ProductInfo.tsx
@@ -184,7 +184,15 @@ function ProductInfo({
           : <OutOfStock sku={sku} />}
       </div>
       <PageFolder activate={device !== "desktop"}>
+        
         {/* Benefities */}
+
+        <div class={`${productBenefits?.length == 0 ? "hidden" : ""}`}>
+          <ProductBenefits
+            productBenefits={productBenefits}
+            adminBenefits={benefits}
+          />
+        </div>
 
         {/* Description card */}
 
