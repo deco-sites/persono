2e9e24ec61599c782e911bb10df410fd47eaedd8
SACCI <sacci@carcara.tech>
2024-04-24 18:52:24 -0300

    update deno

--- sections/Product/ProductShelf.tsx
+++ sections/Product/ProductShelf.tsx
@@ -1,4 +1,4 @@
-export { default } from "$store/components/product/ProductShelf.tsx";
+export { default, loader  } from "$store/components/product/ProductShelf.tsx";
 
 export function LoadingFallback() {
   return (
