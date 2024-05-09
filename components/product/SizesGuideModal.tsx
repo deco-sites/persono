dadcb925e9b7d3dff430d0c1c3fc687fbbf920b2
viniciusventura29 <viniciusventura29@gmail.com>
2024-05-09 00:26:47 -0300

    fix close Icon and change div to ul, li

--- components/product/SizesGuideModal.tsx
+++ components/product/SizesGuideModal.tsx
@@ -1,5 +1,6 @@
 import { SizeGuide } from "deco-sites/persono/loaders/Layouts/SizeGuide.tsx";
 import { Signal } from "@preact/signals";
+import Icon from "deco-sites/persono/components/ui/Icon.tsx";
 
 interface Props {
   segment: string | undefined;
@@ -52,38 +53,37 @@ export function SizesGuideModal(
         </div>
       </>
     );
-  } else {
-    return (
-      <div class="bg-white sm:h-auto h-full w-full rounded-none flex flex-col sm:rounded-lg py-0 px-4">
-        <div class="flex justify-between items-center py-2 border-b">
-          <h3 class="font-medium text-xl">Guia de tamanhos</h3>
-          <div class=" mt-0">
-            <label
-              onClick={() => {
-                showMobile.value = false;
-              }}
-              class="btn btn-sm btn-circle btn-ghost"
-            >
-              ✕
-            </label>
-          </div>
-        </div>
-        <p class="pt-4 pb-8 text-sm text-gray-600">
-          Escolha de acordo com o tamanho do(a) {segment ?? "produto"}
-        </p>
-        <div class="flex flex-col divide-y mb-14">
-          {sizes?.map((s) => (
-            <div class="flex py-4 text-base justify-between">
-              <span>{s.name}</span>
-              <div class="flex flex-col gap-3">
-                {s.value.split("/").map((sizeSplited) => (
-                  <span class="text-gray-600">{sizeSplited}</span>
-                ))}
-              </div>
-            </div>
-          ))}
+  }
+  return (
+    <div class="bg-white sm:h-auto h-full w-full rounded-none flex flex-col sm:rounded-lg py-0 px-4">
+      <div class="flex justify-between items-center py-2 border-b">
+        <h3 class="font-medium text-xl">Guia de tamanhos</h3>
+        <div class=" mt-0">
+          <label
+            onClick={() => {
+              showMobile.value = false;
+            }}
+            class="btn btn-sm btn-circle btn-ghost"
+          >
+            <Icon width={20} height={20} id="XMark"/>
+          </label>
         </div>
       </div>
-    );
-  }
+      <p class="pt-4 pb-8 text-sm text-gray-600">
+        Escolha de acordo com o tamanho do(a) {segment ?? "produto"}
+      </p>
+      <ul class="flex flex-col divide-y mb-14">
+        {sizes?.map((s) => (
+          <li class="list-none flex py-4 text-base justify-between">
+            <span>{s.name}</span>
+            <div class="flex flex-col gap-3">
+              {s.value.split("/").map((sizeSplited) => (
+                <span class="text-gray-600">{sizeSplited}</span>
+              ))}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
