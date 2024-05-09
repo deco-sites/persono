a8c71208e4009b42ed7960c67a73079e815d0fab
viniciusventura29 <viniciusventura29@gmail.com>
2024-05-08 21:05:59 -0300

    accordionItem

--- sections/institutional/AccordionItem.tsx
+++ sections/institutional/AccordionItem.tsx
@@ -1,1 +1,26 @@
-export { default } from "$store/islands/AccordionItem.tsx";
+import ProductInfoCollapse from "deco-sites/persono/islands/ProductInfoCollapse.tsx";
+
+export interface Props {
+  /**
+   * @description Content will be rendered as markdown.
+   * @format rich-textø
+   */
+  /** @format html */
+  content: string;
+  title: string;
+}
+
+export default function AccordionItem({ title, content }: Props) {
+  return (
+    <div class=" border-b">
+    <ProductInfoCollapse title={title}>
+         <div
+          class={`px-4`}
+          dangerouslySetInnerHTML={{
+            __html: content.replace(/<p>|<\/p>/g, "\n"),
+          }}
+        />
+    </ProductInfoCollapse>
+    </div>
+  );
+}
