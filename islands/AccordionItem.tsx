f0ac78fd562f052842d3cde76edf05a261890901
viniciusventura29 <viniciusventura29@gmail.com>
2024-05-15 18:33:01 -0300

    refact: accordionItem

--- islands/AccordionItem.tsx
+++ islands/AccordionItem.tsx
@@ -1,37 +1,46 @@
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 
 export interface Props {
   title?: string;
   content: string;
 }
 
-const AccordionItem = (
-  { title, content }: Props,
-) => {
+const AccordionItem = ({ title, content }: Props) => {
   const [openCollapse, setOpenCollapse] = useState(false);
-  const [showContent, setShowContent] = useState(false);
+  const [collapseHeight, setCollapseHeight] = useState("0px");
+
+  useEffect(() => {
+    if (openCollapse) {
+      const collapse = document.getElementById(title ?? "");
+      if (collapse) {
+        setCollapseHeight(`${collapse.scrollHeight}px`);
+      }
+    } else {
+      setCollapseHeight("0px");
+    }
+  }, [openCollapse, title]);
 
   return (
     <div class="w-full border-b">
       <button
-        onClick={() => {
-          setOpenCollapse(!openCollapse);
-          setShowContent(!showContent);
-        }}
-        class="flex w-full p-4 items-center justify-between pl-0  "
+        onClick={() => setOpenCollapse(!openCollapse)}
+        class="flex w-full p-4 items-center justify-between pl-0"
       >
         <span class="w-full text-start text-base font-normal">{title}</span>
         <span class="text-2xl text-primary">{openCollapse ? "-" : "+"}</span>
       </button>
       <section
-        class={`w-full flex flex-col transition-[max-height] duration-300 ease-in-out ${
-          openCollapse ? "max-h-screen " : "max-h-0 "
-        } overflow-hidden`}
+        id={title}
+        style={{
+          maxHeight: collapseHeight,
+          overflow: "hidden",
+          transition: "max-height 0.5s ease-in-out",
+        }}
       >
         <div
-          class={` px-4 pb-4 ${
-            showContent ? "opacity-100" : "opacity-0 "
-          } transition-opacity duration-300 ease-linear`}
+          class={`px-4 pb-4 ${
+            openCollapse ? "opacity-100" : "opacity-0"
+          } transition-opacity duration-500 ease-linear`}
           dangerouslySetInnerHTML={{
             __html: content.replace(/<p>|<\/p>/g, "\n"),
           }}
